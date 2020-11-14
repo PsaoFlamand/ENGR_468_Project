2bd8c622c1f7a440c4b6abd21b3ce49b432a89f5
anthony15678 <73967366+anthony15678@users.noreply.github.com>
2020-11-14 02:58:41 -0800

    Add files via upload

---
+++ Memory_control/Memory Control.v
@@ -0,0 +1,30 @@
+// master code of memory control module 
+module memory_control (op_code, SR1, SR2, STR, LDR, RW, data_reg, add_bus, data_bus,pc);
+// pc instruction access, part of memory control 
+//8 bit counter 
+input [3:0] op_code;
+input[31:0] SR1, SR2,pc;
+output reg LDR,STR, RW;
+output reg [31:0] data_reg, add_bus, data_bus;
+
+
+always @*
+begin 
+if(op_code==4'b1101)   //LDR
+begin
+LDR=1;
+RW=1;
+Address_bus;
+LDR_mux;
+end
+
+else if (op_code==4'b1110)//STR
+STR=1;
+RW=0;
+Address_bus;
+
+else 
+PC_access
+end 
+//LDR_mux(LDR, address,
+endmodule 
