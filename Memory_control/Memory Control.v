83fde8be74e7c218f96e0fa855107b59a2232e8e
anthony15678 <73967366+anthony15678@users.noreply.github.com>
2020-11-14 12:57:12 -0800

    Update Memory Control.v

--- Memory_control/Memory Control.v
+++ Memory_control/Memory Control.v
@@ -1,9 +1,12 @@
 // master code of memory control module 
-module memory_control (op_code, SR1, SR2, STR, LDR, RW, data_reg, add_bus, data_bus,pc);
 // pc instruction access, part of memory control 
-//8 bit counter 
+module memory_control (op_code, SR1, SR2, STR, LDR, RW, data_reg, add_bus, data_bus,pc,alu_result,Clk, Reset);
+
+
 input [3:0] op_code;
-input[31:0] SR1, SR2,pc;
+input[31:0] SR1, SR2, alu_result;
+input [7:0]pc;
+input Clk, Reset;
 output reg LDR,STR, RW;
 output reg [31:0] data_reg, add_bus, data_bus;
 
@@ -15,8 +18,8 @@ if(op_code==4'b1101)   //LDR
 begin
 LDR=1;
 RW=1;
-Address_bus;
-LDR_mux;
+Address_bus AB1(SR1, pc, LDR, STR, add_bus);
+LDR_mux LD1(LDR, data_reg, data_bus,alu_result);
 end
 
 else if (op_code==4'b1110)//STR
@@ -24,11 +27,12 @@ begin
 
 STR=1;
 RW=0;
-Address_bus;
+data_bus=SR2;
+Address_bus AD2 (SR1, pc, LDR, STR, add_bus);
 end 
 
 else 
-PC_access;
-end 
+PC_access PC1(Clk, Reset, pc);
 
+end 
 endmodule 
