61c970f1c00eaefa16c48cee73b643d4bc15c2b6
Centurion215 <50506777+Centurion215@users.noreply.github.com>
2020-11-21 14:51:26 -0800

    Update Register_Master.v

--- Register_Bank/Register_Master.v
+++ Register_Bank/Register_Master.v
@@ -1,4 +1,4 @@
-module Register_bank(Dest, Source1, Source2, ldr_in, clk, Result_1, Result_2, r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15);
+module Register_bank(Dest, Source1, Source2, ldr_in, clk, Result_1, Result_2);
 
 input [3:0] Dest, Source1, Source2;
 input [31:0] ldr_in;
@@ -7,7 +7,7 @@ output [31:0] Result_1, Result_2;
 input clk;
 
 wire [15:0] en;
-output [31:0] r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15;
+wire [31:0] r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15;
 
 decoder d1(clk, Dest, en);
 reg_bank R1(clk, en, ldr_in, r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15);
