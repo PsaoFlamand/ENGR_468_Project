f7a2ef0660eabcb6d29c0c83b42babef65c4da0e
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-21 18:09:46 -0800

    tries

--- Master_CPU/Assembled/Master_CPU.v
+++ Master_CPU/Assembled/Master_CPU.v
@@ -14,8 +14,8 @@ reg [3:0] Flag; //ALU
 wire [15:0] IV_Mov;
 
 wire [7:0] pc; // mem
-wire [31:0] reg_data;// mem
-
+reg [31:0] reg_data_reg;// mem
+wire [31:0] reg_data_mem;
 
 reg Enable; //RAM STUFF
 reg RW_ram;
@@ -36,13 +36,14 @@ wire [31:0] r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15
 
 reg Reset;
 RAM ram(Enable,RW_ram,Address_in,In,Out);
-Register_bank regbank(destination, source_1, source_2, reg_data, Clk, Result_1, Result_2);
-memory_control memcontrol(Result_1, Result_2, OpCode, RW_mem, Address_out, reg_data, LDR, STR ,LDR_out, STR_in, Counter, Reset, Clk, pc, Result);
+Register_bank regbank(destination, source_1, source_2, reg_data_reg, Clk, Result_1, Result_2);
+memory_control memcontrol(Result_1, Result_2, OpCode, RW_mem, Address_out, reg_data_mem, LDR, STR ,LDR_out, STR_in, Counter, Reset, Clk, pc, Result_mem);
 MASTER_ALU master(Result_1, Result_2, IV_Shiftror, IV_Mov, OpCode, Cond, S, Result, Flag, New_Flag);
 
 //always @(posedge Clk)
 //assign LDR_out=Result;
-//assign Result_mem=Result;
+//assign Result_mem=10;
+
 assign instruction=Out;
 assign Cond = instruction[31:28]; 
 assign OpCode =  instruction[27:24];
@@ -60,8 +61,10 @@ initial
 begin  
 Clk=1;
 Reset=1;
-//Reg1=Result_1;
-//Reg2=Result_2;
+Reg1=10;
+Reg2=10;
+
+reg_data_reg=reg_data_mem;
 $readmemh("data_h.txt", ram.Mem);
    Enable =0;   RW_ram=1;	Address_in=32'd0;	
 #5 Enable =1;   RW_ram=1;	Address_in=32'd0;	
@@ -82,7 +85,7 @@ begin
 
 $monitor($time, "reg_data:%d Reg1:%d Reg2:%d Cond:%b S:%b destination:%b source_2:%b source_1:%b IV_ShiftRor:%d IV_Mov:%d address:%b OpCode:%b Result:%d flag :%b \n \n \n R0=%d, R1=%d, R2=%d, R3=%d, R4=%d R5=%d, R6=%d, R7=%d, R8=%d, R9=%d, R10=%d, R11=%d, R12=%d, R13=%d, R14=%d,R15=%d \n \n ",
 
- reg_data, Result_1, Result_2,Cond,S,destination,source_2, source_1, IV_ShiftRor, IV_Mov,Address_in, Out, Result,New_Flag, r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15);
+ reg_data_mem, Result_1, Result_2,Cond,S,destination,source_2, source_1, IV_ShiftRor, IV_Mov,Address_in, OpCode, Result,New_Flag, r0, r1, r2, r3, r4, r5, r6 ,r7, r8, r9, r10, r11, r12, r13, r14, r15);
 
 end 
 
