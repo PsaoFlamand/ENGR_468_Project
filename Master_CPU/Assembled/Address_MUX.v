f721d04c9234644c6b7c58a63b677911aee0024d
anthony15678 <73967366+anthony15678@users.noreply.github.com>
2020-11-25 15:53:01 -0800

    Update Address_MUX.v

--- Master_CPU/Assembled/Address_MUX.v
+++ Master_CPU/Assembled/Address_MUX.v
@@ -3,13 +3,14 @@ module Address_bus(out, in1, sel, Reset);
 input sel, Reset;
 input [31:0] in1;
 output reg [31:0] out;
-
+reg [7:0]pc;
 
 always @*
 begin 
 
 if (sel)
 begin	
+pc=0;
 out =in1;
 end 
 /*else if (! Reset)
@@ -18,11 +19,10 @@ end
 	end*/
 else
 	begin
-	out=out+1'b1;
+	pc=pc+1'b1;
+	out=pc;
 	end
 		
 end 
 
 endmodule 
-
-
