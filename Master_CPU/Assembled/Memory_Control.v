8acba691699c7536d70c170407314e88cfa234a7
anthony15678 <73967366+anthony15678@users.noreply.github.com>
2020-11-25 16:55:46 -0800

    Update Memory_Control.v

--- Master_CPU/Assembled/Memory_Control.v
+++ Master_CPU/Assembled/Memory_Control.v
@@ -14,44 +14,45 @@ reg Reset;
 always @(posedge Clk)
 //always @*
 	case (op_code)
- 		4'b1100: //ADR
-		begin
+ 	4'b1100: //ADR
+	begin
 		sel_add=0;
 		sel_LDR=0;
 		RW=0;
+		Reset=0;
 		
-
 		RAM_in=1'bx; 
 		address_out= out_add; 
 		reg_data=out_LDR;	
-		end 
+	end 
 	
-
-		4'b1110: // STR
-		begin
+	4'b1110: // STR
+	begin
 		sel_add=1;
 		sel_LDR=0;
 		RW=0;
+		Reset=0;
 		
 		RAM_in=SR2; 
 		address_out= out_add; 
 		reg_data=1'bz;	
-		end 
+	end 
 
-		4'b1101: //LDR
+	4'b1101: //LDR
 	begin
 		sel_add=1;
 		sel_LDR=1;
 		RW=1;
+		Reset=0;
 		
 		address_out= out_add; 
 		reg_data=out_LDR;	
 	end 
-		default: // ALU instructions
+
+	default: // ALU instructions
 	begin 
 		sel_add=0;
 		sel_LDR=0;
-		Reset=1;
 		RW=0;
 		
 		address_out=out_add;
@@ -59,6 +60,6 @@ always @(posedge Clk)
 	end 
 endcase
 
-Address_bus addbus(out_add, SR1, sel_add, Reset);
+Address_bus addbus(out_add, SR1, sel_add,Reset);
 LDR_MUX LDRMux(out_LDR, ALU_result, RAM_out, sel_LDR);
 endmodule 
